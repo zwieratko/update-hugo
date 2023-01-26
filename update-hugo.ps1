0dad02d35ed49d01d2a822763630a1bb2a7ce544
Radovan Snirc <zwieratko1@gmail.com>
2023-01-26 21:02:50 +0100

    refactor(ps1): improve the logic, cleanup

--- update-hugo.ps1
+++ update-hugo.ps1
@@ -1,19 +1,43 @@
 <# 
-    .NOTES 
-    =========================================================================== 
-     Created with: love by hand in Visual Studio Code 1.63.2 
-     Created on: 27/12/2021 10:33 AM 
-     Created by: Rado van Zwieratko 
-     Organization: zwieratko.sk 
-     Filename: update-hugo.ps1 
-    =========================================================================== 
-    .DESCRIPTION 
-     Downloading and installing of binary for Hugo static site generator. 
+.SYNOPSIS
+Hugo downloader
+
+.DESCRIPTION
+Downloading and installing of binary for Hugo static site generator.
+
+.NOTES
+===========================================================================
+Created with: love by hand in Visual Studio Code 1.63.2
+Created on: 27/12/2021 10:33 AM
+Created by: Rado van Zwieratko
+Organization: zwieratko.sk
+Filename: update-hugo.ps1
+===========================================================================
 #>
 
+#PARAMS
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $increaseDebugVerbosity
+)
+
+Write-Host "Wait a seconds, please. Everything is preparing..."
+Write-Debug "Debug verbosity is increased! Sorry."
+
+#VARIABLES
 $current_path_at_start = (Get-Item .).FullName
+# Detect if Hugo is installed and which version
+if (Get-Command hugo.exe -ErrorAction SilentlyContinue) {
+    $installed_hugo_version = ((((hugo version) -split " ")[1]) -split "-")[0]
+}
+else {
+    $installed_hugo_version = 0
+}
 $base_path = "https://github.com/gohugoio/hugo/releases/download/"
-#$new_version = "v0.106.0"
+# Detect latest available version og Hugo at GitHub repository
+# Format = "v0.106.0"
 $params = @{
     useBasicParsing = $true
     URI             = "https://api.github.com/repos/gohugoio/hugo/releases/latest"
@@ -23,44 +47,69 @@ $hugo_extended = "/hugo_extended_"
 $hugo_version = $new_version.substring(1)
 $hugo_arch = "_Windows-amd64.zip"
 
-Write-Host $base_path$new_version$hugo_extended$hugo_version$hugo_arch
+Write-Debug $base_path$new_version$hugo_extended$hugo_version$hugo_arch
 
-Write-Host "................................"
-Write-Host "Download and install Hugo binary"
-Write-Host "................................"
-Write-Host "New version is: "$new_version
-$answer = Read-Host "Is it correct (y/n) ?"
+
+Write-Host "......................................."
+Write-Host "Download and install latest Hugo binary"
+Write-Host "......................................."
+Write-Host "Version of Hugo installed at system is :", $installed_hugo_version
+Write-Host "Latest available version at Github  is :", $new_version
+
+if ($installed_hugo_version -eq $new_version) {
+    Write-Host "We already have installed latest version of Hugo."
+    $answer = Read-Host "Do you want to install other version (y/n) ?"
+    if ($answer -ne 'y') {
+        Write-Host "End."
+        Exit
+    }
+    else {
+        $answer = 'n'
+        while ($answer -ne 'y') {
+            $desired_version = Read-Host "Type the desired version number, please (for example v0.109.0)"
+            Write-Host "Selected version is: "$desired_version
+            $answer = Read-Host "Is it correct (y/n) ?"
+            if ($desired_version -match '^v0\.\d{2,3}\.\d') {
+                $answer = 'y'
+            }
+            else {
+                $answer = 'n'
+            }
+        }
+        if ($answer -eq 'y') {
+            Write-Host "OK."
+            $new_version = $desired_version
+            $hugo_version = $new_version.substring(1)
+        }
+    }
+}
+
+$answer = Read-Host "Do you want to continue installing version $hugo_version (y/n) ?"
 if ($answer -eq 'y') {
     Write-Host "OK."
 }
 else {
-    $corrected_version = Read-Host "Type the correct version number, please (for example v0.91.2)"
-    Write-Host "New version is: "$corrected_version
-    $answer = Read-Host "Is it correct (y/n) ?"
-    if ($answer -eq 'y') {
-        Write-Host "OK."
-        $new_version = $corrected_version
-        $hugo_version = $new_version.substring(1)
-    }
-    else {
-        Write-Host "Sorry. We have to exit."
-        Write-Host "................................"
-        exit 1
-    }
+    Write-Host "Sorry. We have to exit."
+    Write-Host "................................"
+    Exit 1
 }
 
 #Set-Location D:\bin
 $local_path = "D:\bin\"
-Write-Host $local_path
-New-Item -ItemType "directory" -Path $local_path -Name $hugo_version -Force
+Write-Debug $local_path
+
+
 #Set-Location $hugo_version
 $checked_path = $local_path + $hugo_version + "\hugo_extended_" + $hugo_version + $hugo_arch
-Write-Host $checked_path
+Write-Debug $checked_path
+
 if (Test-Path -Path $checked_path -PathType Leaf) {
     Write-Host "OK." -ForegroundColor DarkGreen -NoNewline
     Write-Host "We already have that version of Hugo archive."
 }
 else {
+    Write-Host "Creating folder..."
+    New-Item -ItemType "directory" -Path $local_path -Name $hugo_version -Force
     Write-Host "Downloading..."
     $url_for_download = $base_path + $new_version + $hugo_extended + $hugo_version + $hugo_arch
     Write-Host $url_for_download
@@ -68,23 +117,27 @@ else {
 }
 
 $checked_path_binary = $local_path + $hugo_version + "\hugo.exe"
-Write-Host $checked_path_binary
+Write-Debug $checked_path_binary
+
 if (Test-Path -Path $checked_path_binary -PathType Leaf) {
     Write-Host "OK." -ForegroundColor DarkGreen -NoNewline
     Write-Host "We already have that version of Hugo binary."
 }
 else {
     Write-Host "Extracting..."
-    Expand-Archive -Path $checked_path -DestinationPath ($local_path + $hugo_version)
+    Expand-Archive -Path $checked_path -DestinationPath ($local_path + $hugo_version) -Force
 }
 
-# Copy Hugo binary to final destination
-Copy-Item $checked_path_binary -Destination "D:\bin\hugo.exe"
+if ($new_version -ne $installed_hugo_version) {
+    # Copy new Hugo binary to final destination
+    Copy-Item $checked_path_binary -Destination "D:\bin\hugo.exe"
+}
 
-$installed_hugo_version = (hugo version).substring(6, 7)
-Write-Host "Installed Hugo version: "$installed_hugo_version
+#$installed_hugo_version = (hugo version).substring(6, 7)
+$installed_hugo_version = ((((hugo version) -split " ")[1]) -split "-")[0]
+Write-Host "Installed Hugo version:", $installed_hugo_version
 
 Set-Location $current_path_at_start
-# https://github.com/gohugoio/hugo/releases/download/v0.91.2/hugo_extended_0.91.2_Windows-64bit.zip
-# D:\bin\0.91.0\hugo_extended_0.91.0_Windows-64bit.zip
-# D:\bin\0.91.0\hugo_extended_0.91.0_Windows-64bit.zip
+
+Write-Host "End."
+Exit
